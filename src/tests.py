981edd6ec3125b34988587912543b2e8dce0e21a
dexXxed <dexxxed.app@gmail.com>
2020-05-25 17:05:21 +0300

    Remade with the help of decorators

--- src/tests.py
+++ src/tests.py
@@ -17,17 +17,13 @@ def timing(f):
 @timing
 def itubee_logic(itubee, plain_text, key):
     enc = itubee.encrypt(plain_text, key)
-    print(enc)
-
     dec = itubee.decrypt(enc, key)
-    print(dec)
+    print(f"{enc=}\n{dec=}", end="\n\n")
 
 
 if __name__ == '__main__':
     itubee = ITUBEE()
 
     itubee_logic(itubee, '01000000000000000000', '00000000000000000080')
-
     itubee_logic(itubee, '00000000000000000000', '00000000000000000000')
-
     itubee_logic(itubee, '6925278951fbf3b25ccc', 'c538bd9289822be43363')
--- src/text_tests.py
+++ src/text_tests.py
@@ -1,17 +1,29 @@
 from itubee.main import ITUBEETEXTS
-import time
+from functools import wraps
+from time import time
 
-start = time.time()
-itubee = ITUBEETEXTS()
 
-key2 = '00000000000000000080'
-plain_text2 = 'Andrey Rocks'
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print(f'func:{f.__name__} args:[{args}, {kw}] Выполнялась: {te - ts} sec')
+        return result
+    return wrap
 
-enc2 = itubee.encrypt_text(plain_text2, key2)
-dec2 = itubee.decrypt_text(enc2, key2)
-print(enc2)
-print(dec2)
-print(dec2 == plain_text2)
 
-end = time.time()
-print(end - start)
+@timing
+def itubee_logic_text(itubee, plain_text, key):
+    enc = itubee.encrypt_text(plain_text, key)
+    dec = itubee.decrypt_text(enc, key)
+    print("".join(enc))
+    print(dec)
+    print(dec == plain_text)
+
+
+if __name__ == '__main__':
+    itubee_text = ITUBEETEXTS()
+
+    itubee_logic_text(itubee_text, 'dexxxed', '00000000000000000080')
